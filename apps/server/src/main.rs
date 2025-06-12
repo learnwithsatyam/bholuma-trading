6024d4aaedeaf272c0507d9f66ca4a87a3cf108a
Satyam Shivhare <mkssshivhare@gmail.com>
2025-06-13 04:22:33 +0530

    created the template api endpoints and functions for use

--- apps/server/src/main.rs
+++ apps/server/src/main.rs
@@ -5,6 +5,32 @@ async fn hello() -> impl Responder {
     HttpResponse::Ok().body("Hello world!")
 }
 
+#[get("/chat")]
+async fn getChat() -> impl Responder {
+    // get the chat data from db
+    // serialise in json 
+    // send to frontend
+}
+
+#[get("/")]
+async fn getStockData() -> impl Responder {
+    // fetch data from zerodha
+    // serialise it in json
+    //send frontend
+}
+
+#[post("/updateChat")]
+async fn updateChat() -> impl Responder {
+    // update the chat in db
+}
+
+#[post("/callback/zerodha")]
+async fun updateAccessToken() -> impl Responder{
+    // capture the request token from request
+    // use apikey and request token to get the access token
+    // store the access token along with time in a json file.
+}
+
 #[post("/echo")]
 async fn echo(req_body: String) -> impl Responder {
     HttpResponse::Ok().body(req_body)
