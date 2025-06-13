31e2cfdee1d91aafeb1f1c01840b0cff434d4d18
Satyam Shivhare <mkssshivhare@gmail.com>
2025-06-13 19:48:24 +0530

    adding some modules in main rs to test how module imports work

--- apps/server/src/main.rs
+++ apps/server/src/main.rs
@@ -1,5 +1,8 @@
 use actix_web::{App, HttpResponse, HttpServer, Responder, get, post, web};
 
+use models::TokenResponse;
+mod models;
+
 #[get("/")]
 async fn hello() -> impl Responder {
     HttpResponse::Ok().body("Hello world!")
@@ -25,10 +28,24 @@ async fn updateChat() -> impl Responder {
 }
 
 #[post("/callback/zerodha")]
-async fun updateAccessToken() -> impl Responder{
+async fn updateAccessToken(query: web::Query<TokenResponse>) -> impl Responder{
     // capture the request token from request
+    let requestToken = &query.request_token;
     // use apikey and request token to get the access token
+    let accessToken = get_access_token(process.env.ZERODHA_API_KEY, requestToken, process.env.ZERODHA_API_SECRET);
     // store the access token along with time in a json file.
+    match access_token_result {
+        Ok(access_token) => {
+            if let Err(e) = save_to_file(&access_token, "BholumaTrading.config.json") {
+                return HttpResponse::InternalServerError()
+                    .body(format!("Failed to save token: {}", e));
+            }
+
+            HttpResponse::Ok().body("✅ Access token saved successfully")
+        }
+        Err(e) => HttpResponse::BadRequest()
+            .body(format!("❌ Failed to fetch access token: {}", e)),
+    }
 }
 
 #[post("/echo")]
