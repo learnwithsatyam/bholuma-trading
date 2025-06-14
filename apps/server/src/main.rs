85e160bf8e99cae479cbf177ebacfb92a92f8734
Satyam Shivhare <mkssshivhare@gmail.com>
2025-06-14 11:47:16 +0530

    initial implementation to add the ticker logic in main.rs file

--- apps/server/src/main.rs
+++ apps/server/src/main.rs
@@ -1,14 +1,17 @@
 mod models;
 mod utils;
 mod zerodhaAuth;
+extern crate kiteconnect;
 
 use actix_web::{get, post, web::{self, get}, App, HttpResponse, HttpServer, Responder};
 use models::TokenResponse::TokenResponse;
 use models::AccessToken::AccessToken;
 use serde_json::value;
-use utils::commonUtilities::{save_to_file, read_from_file, get_access_token_validity};
+use utils::commonUtilities::{save_to_file, get_access_token_validity, get_fresh_access_token};
 use zerodhaAuth::kiteconnect::get_kite_connect;
-use std::env::var;
+use std::{env::var, thread};
+use kiteconnect::ticker::{KiteTicker, KiteTickerHandler, WebSocketHandler};
+use zerodhaAuth::{*};
 
 #[get("/zerodha/callback")]
 async fn set_zerodha_access_token(data: web::Query<TokenResponse>) -> impl Responder {
@@ -41,8 +44,32 @@ async fn set_zerodha_access_token(data: web::Query<TokenResponse>) -> impl Respo
      }
 }
 
+fn start_ticker(api_key: String, access_token: String) {
+    let mut ticker = KiteTicker::new(&api_key, &access_token);
+
+    let custom_handler = kiteTicker::CustomHandler{ count : 0};
+
+    println!("[ticker] Connecting with access token: {}", &access_token);
+
+    // this blocks until disconnected
+    ticker.connect(custom_handler, None);
+}
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+
+    let api_key = std::env::var("ZERODHA_API_KEY").unwrap_or_else(|error| panic!("ZERODHA API KEY NOT FOUND!!"));
+
+    // ✅ Spawn the ticker in its own thread
+    thread::spawn(move || {
+        loop {
+            let access_token = get_fresh_access_token(var("ACCESS_TOKEN_CONFIG_FILE").unwrap_or_default().as_str());
+            start_ticker(api_key.clone(), access_token.access_token);
+            println!("[ticker] Disconnected, retrying in 2s...");
+            std::thread::sleep(std::time::Duration::from_secs(2));
+        }
+    });
+
     HttpServer::new(|| {
         App::new()
     })
