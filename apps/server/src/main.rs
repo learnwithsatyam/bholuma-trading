d906b47ff909755402edd5803e36a759b65d6369
Satyam Shivhare <mkssshivhare@gmail.com>
2025-06-14 18:20:18 +0530

    making changes in the main.rs file and now we can properly use the kite ticker and generate and store the access token

--- apps/server/src/main.rs
+++ apps/server/src/main.rs
@@ -3,79 +3,113 @@ mod utils;
 mod zerodhaAuth;
 extern crate kiteconnect;
 
-use actix_web::{get, post, web::{self, get}, App, HttpResponse, HttpServer, Responder};
-use models::TokenResponse::TokenResponse;
-use models::AccessToken::AccessToken;
-use serde_json::value;
-use utils::commonUtilities::{save_to_file, get_access_token_validity, get_fresh_access_token};
-use zerodhaAuth::kiteconnect::get_kite_connect;
-use std::{env::var, thread};
-use kiteconnect::ticker::{KiteTicker, KiteTickerHandler, WebSocketHandler};
-use zerodhaAuth::{*};
+use actix_web::{
+    App, HttpResponse, HttpServer, Responder, get, post,
+    web::{self, get},
+};
 use dotenv::dotenv;
+use kiteconnect::ticker::{KiteTicker, KiteTickerHandler, WebSocketHandler};
+use models::AccessToken::AccessToken;
+use models::TokenResponse::TokenResponse;
+use serde_json::value;
+use std::{env::var, f64::consts::E, thread};
+use utils::commonUtilities::{get_access_token_validity, get_fresh_access_token, save_to_file};
+use zerodhaAuth::kiteconnect::get_kite_connect;
+use zerodhaAuth::*;
+
+use crate::models::AccessToken::TokenData;
 
 #[get("/zerodha/callback")]
 async fn set_zerodha_access_token(data: web::Query<TokenResponse>) -> impl Responder {
-    let mut token_response = TokenResponse{
-        request_token : data.request_token.clone(),
-        status : data.status.clone(),
-        action : data.action.clone(),
-        type_ : data.type_.clone(),        
+    let mut token_response = TokenResponse {
+        request_token: data.request_token.clone(),
+        status: data.status.clone(),
+        action: data.action.clone(),
+        type_: data.type_.clone(),
     };
     let mut kiteconnect = get_kite_connect().unwrap();
     let api_secret = var("ZERODHA_API_SECRET").unwrap();
-    let session = kiteconnect.generate_session(&token_response.request_token, api_secret.as_str().clone());
+    let session =
+        kiteconnect.generate_session(&token_response.request_token, api_secret.as_str().clone());
 
-     match session {
+    match session {
         Ok(access_token) => {
             // save access token to file
+            let access_token_data: AccessToken = serde_json::from_value(access_token).unwrap();
+
             let access_token_response = AccessToken {
-                access_token: access_token.to_string(),
-                valid_till: get_access_token_validity(),
+                data: access_token_data.data,
+                status: access_token_data.status,
             };
 
-            match save_to_file(&access_token_response, var("ACCESS_TOKEN_CONFIG_FILE").unwrap().as_str()) {
+            match save_to_file(
+                &access_token_response,
+                var("ACCESS_TOKEN_CONFIG_FILE").unwrap().as_str(),
+            ) {
                 Ok(_) => HttpResponse::Ok().body(format!("saved access token.")),
-                Err(_) => HttpResponse::InternalServerError().body(format!("Failed to save access token")),
+                Err(_) => {
+                    HttpResponse::InternalServerError().body(format!("Failed to save access token"))
+                }
             }
         }
         Err(_) => {
             HttpResponse::InternalServerError().body(format!("Could not generate access token"))
         }
-     }
+    }
 }
 
 fn start_ticker(api_key: String, access_token: String) {
     let mut ticker = KiteTicker::new(&api_key, &access_token);
 
-    let custom_handler = kiteTicker::CustomHandler{ count : 0};
+    let custom_handler = kiteTicker::CustomHandler { count: 0 };
 
     println!("[ticker] Connecting with access token: {}", &access_token);
 
     // this blocks until disconnected
-    ticker.connect(custom_handler, None);
+    match ticker.connect(custom_handler, None) {
+        Ok(_) => println!("[ticker] Connection closed gracefully."),
+        Err(e) => eprintln!("[ticker] Connection error: {:?}", e),
+    }
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-
     dotenv().ok();
-    let api_key = std::env::var("ZERODHA_API_KEY").unwrap_or_else(|error| panic!("ZERODHA API KEY NOT FOUND!!"));
+    let api_key = std::env::var("ZERODHA_API_KEY")
+        .unwrap_or_else(|error| panic!("ZERODHA API KEY NOT FOUND!!"));
 
     // ✅ Spawn the ticker in its own thread
     thread::spawn(move || {
+        let mut connected: bool = false;
         loop {
-            let access_token = get_fresh_access_token(var("ACCESS_TOKEN_CONFIG_FILE").unwrap_or_default().as_str());
-            start_ticker(api_key.clone(), access_token.access_token);
-            println!("[ticker] Disconnected, retrying in 2s...");
+            let access_token = get_fresh_access_token(
+                var("ACCESS_TOKEN_CONFIG_FILE").unwrap_or_default().as_str(),
+            );
+            match access_token {
+                Ok(token) => {
+                    if (token.data.access_token == "" || connected) {
+                        continue;
+                    }
+                    let mut ticker = KiteTicker::new(&api_key, &token.data.access_token);
+                    let custom_handler = kiteTicker::CustomHandler { count: 0 };
+                    match ticker.connect(custom_handler, None) {
+                        Ok(_) => connected = true,
+                        Err(e) => eprintln!("[ticker] Connection error: {:?}", e),
+                    }
+                }
+                Err(e) => {
+                    eprintln!("[ticker] no access token error: {:?}", e)
+                }
+            }
+
+            //start_ticker(api_key.clone(), access_token.access_token);
+            //println!("[ticker] Disconnected, retrying in 2s...");
             std::thread::sleep(std::time::Duration::from_secs(2));
         }
     });
 
-    HttpServer::new(|| {
-        App::new()
-    })
-    .bind(("127.0.0.1", 8080))?
-    .run()
-    .await
+    HttpServer::new(|| App::new().service(set_zerodha_access_token))
+        .bind(("127.0.0.1", 8080))?
+        .run()
+        .await
 }
