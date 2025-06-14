076e9083ffa62f57b1c2926acc505cbfe005a025
Satyam Shivhare <mkssshivhare@gmail.com>
2025-06-14 06:20:04 +0530

    setup for kiteconnect module

--- apps/server/src/zerodhaAuth/kiteconnect.rs
+++ apps/server/src/zerodhaAuth/kiteconnect.rs
@@ -1,23 +1,14 @@
 extern crate kiteconnect;
 extern crate serde_json as json;
 
-mod commonUtilities;
-
 use kiteconnect::connect::KiteConnect;
 use std::error::Error;
+use std::env::var;
+use crate::utils::commonUtilities::read_from_file;
 
-fn get_kite_connect(requestToken: &str) -> Result<KiteConnect, Box<dyn Error>> {
-    let mut kiteconnect = KiteConnect::new(process.env.ZERODHA_API_KEY, process.env.ZERODHA_API_SECRET);
-
-    // Show login URL
-    let login_url = kiteconnect.login_url();
-    println!("Login at: {}", login_url);
-
-    let accessTokenData = commonUtilities::read_from_file("BholumaTrading.config.json");
-
-    // After manually logging in and getting the request token, use it:
-    let session = kiteconnect.generate_session(requestToken, process.env.ZERODHA_API_SECRET)?;
-    println!("Access Token Response: {:?}", session);
-
+pub fn get_kite_connect() -> Result<KiteConnect, Box<dyn Error>> {
+    let api_key = var("ZERODHA_API_KEY")?;
+    let api_secret: String = var("ZERODHA_API_SECRET")?;
+    let mut kiteconnect = KiteConnect::new(api_key.as_str(), api_secret.as_str().clone());
     Ok(kiteconnect)
 }
