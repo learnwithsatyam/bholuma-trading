db820f8d693b377e31b810df13449dc54dbb7a71
Satyam Shivhare <mkssshivhare@gmail.com>
2025-06-14 06:19:44 +0530

    added utility function to get access token validity

--- apps/server/src/utils/commonUtilities.rs
+++ apps/server/src/utils/commonUtilities.rs
@@ -1,18 +1,23 @@
 use std::fs::{self, File};
 use std::io::Write;
 use serde_json;
+use time::{OffsetDateTime, Duration};
+use crate::models::AccessToken::AccessToken;
 
-use crate::models::TokenResponse::TokenResponse;
-
-pub fn save_to_file(data: &TokenResponse, filename: &str) -> std::io::Result<()> {
+pub fn save_to_file(data: &AccessToken, filename: &str) -> std::io::Result<()> {
     let json = serde_json::to_string_pretty(data)?; // pretty or just `to_string`
     let mut file = File::create(filename)?;
     file.write_all(json.as_bytes())?;
     Ok(())
 }
 
-pub fn read_from_file(filename: &str) -> std::io::Result<TokenResponse> {
+pub fn read_from_file(filename: &str) -> std::io::Result<AccessToken> {
     let json = fs::read_to_string(filename)?;
-    let tokenData: TokenResponse = serde_json::from_str(&json)?;
-    Ok(tokenData)
+    let token_data: AccessToken = serde_json::from_str(&json)?;
+    Ok(token_data)
+}
+
+pub fn get_access_token_validity() -> OffsetDateTime{
+    // access token is valid only for 24 hours
+    OffsetDateTime::now_utc() + Duration::hours(24) 
 }
